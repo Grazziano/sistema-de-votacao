c8a0d7a432ba73f177e7041dd59437185b80e759
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-02-29 22:37:21 -0300

    feat: implementing the Voter Person class

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -5,6 +5,20 @@ package com.betrybe.sistemadevotacao;
  *
  * @author Grazziano Fagundes
  */
-public class PessoaEleitora {
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    super.setNome(nome);
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
