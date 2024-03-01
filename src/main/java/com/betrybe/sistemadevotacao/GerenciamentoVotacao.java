8dcc36d3f4d1cfd55ec8136b4c45117651ffc188
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-02-29 23:26:41 -0300

    feat: implementing Voting Management class

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -9,18 +9,46 @@ import java.util.ArrayList;
  */
 public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
-  private ArrayList<String> pessoasCandidatas;
-  private ArrayList<String> pessoasEleitoras;
-  private ArrayList<String> cpfsComputados;
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<PessoaEleitora>();
+  private ArrayList<String> cpfsComputados = new ArrayList<String>();
+
+  public ArrayList<PessoaCandidata> getPessoasCandidatas() {
+    return pessoasCandidatas;
+  }
+
+  public ArrayList<PessoaEleitora> getPessoasEleitoras() {
+    return pessoasEleitoras;
+  }
+
+  public ArrayList<String> getCpfsComputados() {
+    return cpfsComputados;
+  }
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (int i = 0; i < pessoasCandidatas.size(); i++) {
+      if (pessoasCandidatas.get(i).getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
+    }
 
+    PessoaCandidata pessoa = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(pessoa);
   }
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (int i = 0; i < pessoasEleitoras.size(); i++) {
+      if (pessoasEleitoras.get(i).getCpf().equals(cpf)) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
+    }
 
+    PessoaEleitora pessoa = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(pessoa);
   }
 
   @Override
