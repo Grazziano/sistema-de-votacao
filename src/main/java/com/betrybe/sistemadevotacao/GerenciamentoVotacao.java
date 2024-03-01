9c67e4acb192d91e1640ddfb98522be7d5db75ba
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-02-29 22:42:19 -0300

    feat: implementing the Voting Management class with attributes

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,10 +1,35 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.ArrayList;
+
 /**
  * Classe para objetos do tipo GerenciamentoVotacao.
  *
  * @author Grazziano Fagundes
  */
-public class GerenciamentoVotacao {
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
+  private ArrayList<String> pessoasCandidatas;
+  private ArrayList<String> pessoasEleitoras;
+  private ArrayList<String> cpfsComputados;
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
